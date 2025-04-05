650f2231278a3bfd4d31abe990ac6c123f9c4cda
addy <4ddy1@proton.me>
2025-04-06 00:05:39 +0600

    еще правки

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
-import {BasePage} from './Base';
 import {Crash} from './crash';
 import {Visual} from './visual';
 import {Product} from './product';
 
-export {BasePage, Crash, Visual, Product};
+export {Crash, Visual, Product};
